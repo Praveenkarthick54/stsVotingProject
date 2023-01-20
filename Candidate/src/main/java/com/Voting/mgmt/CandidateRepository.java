46892eb64b677d46efd6cae0f51b3cfae77989b5
Praveenkarthick54 <120189897+Praveenkarthick54@users.noreply.github.com>
2023-01-20 19:23:41 +0530

    Delete Candidate/src/main/java/com/Voting/mgmt directory

--- Candidate/src/main/java/com/Voting/mgmt/CandidateRepository.java
+++
@@ -1,9 +0,0 @@
-package com.Voting.mgmt;
-
-import java.util.Optional;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CandidateRepository extends JpaRepository<Candidate, Long> {
-    Optional<Candidate> findByName(String name);
-}
